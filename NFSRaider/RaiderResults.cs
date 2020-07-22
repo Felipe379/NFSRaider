9a0d0408a0651eb20d1194a2199337ee4913fb37
Felipe <felipecamargo379@gmail.com>
2020-07-22 20:28:12 -0300

    Improvements in unhashing (#1)
    Co-authored-by: Felipe Camargo <felipe.camargo@flipdigital.com.br>

---
+++ NFSRaider/RaiderResults.cs
@@ -0,0 +1,9 @@
+﻿namespace NFSRaider
+{
+    public class RaiderResults
+    {
+        public uint Hash { get; set; }
+        public string Value { get; set; }
+        public bool IsKnown { get; set; }
+    }
+}
