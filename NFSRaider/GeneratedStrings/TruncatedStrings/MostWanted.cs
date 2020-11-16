520f3d7d0214834a2e9b1888368e3811b0c93e4f
Felipe de Camargo <felipecamargo379@gmail.com>
2020-11-16 20:19:52 -0300

    Add more texture strings

--- NFSRaider/GeneratedStrings/TruncatedStrings/MostWanted.cs
+++ NFSRaider/GeneratedStrings/TruncatedStrings/MostWanted.cs
@@ -7,12 +7,8 @@ namespace NFSRaider.GeneratedStrings.TruncatedStrings
         public static Dictionary<uint, string> BinHashesTextures = new Dictionary<uint, string>()
         {
 { 0x6B5489AC, "SKY_MIDDAY_A_ENVIRONMEN" },
-{ 0x7CE22D59, "ARC_OFFICEMETALPANELLIN" },
-{ 0x7FB79429, "ARC_WALLFINANCIALREDBRI" },
-{ 0x8B012045, "SHD_CP_FISHINGVILLAGE_T" },
 { 0x9345D805, "ARC_WALLFINANCIALREDBRI" },
 { 0xF0C9A9A4, "ARC_WALLFINANCIALREDBRI" },
-{ 0xF738F910, "PAN_COLLEGETOWN_MOUNT_S" },
         };
     }
 }
