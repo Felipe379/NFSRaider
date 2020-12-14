09e235d73db80ee14a7137107b03c4a6f2ed175d
Felipe de Camargo <felipecamargo379@gmail.com>
2020-12-13 22:56:58 -0300

    - Add UC cars; - Change the logic to generate a few files strings;

--- NFSRaider/GeneratedStrings/Files/BuildFiles.cs
+++ NFSRaider/GeneratedStrings/Files/BuildFiles.cs
@@ -8,6 +8,24 @@ namespace NFSRaider.GeneratedStrings.Files
         {
             var files = new HashSet<string>(Files.List);
 
+            foreach (var language in Localized.Languages.List)
+            {
+                foreach (var file in Localized.Files.List)
+                {
+                    files.Add(file.Replace("(EnglishName)", language.EnglishName)
+                                  .Replace("(ThreeLettersCode)", language.ThreeLettersCode)
+                                  .Replace("(TwoLettersCode)", language.TwoLettersCode));
+                }
+            }
+
+            foreach (var car in Shared.Cars.List)
+            {
+                foreach (var file in Cars.Files.List)
+                {
+                    files.Add(file.Replace("(Car)", car));
+                }
+            }
+
             return files;
         }
     }
---
+++ NFSRaider/GeneratedStrings/Files/Localized/Languages.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace NFSRaider.GeneratedStrings.Files.Localized
+{
+    public static class Languages
+    {
+        public static readonly List<Language> List = new List<Language>
+        {
+            new Language { EnglishName = "BRAZILIAN_PORTUGUESE", ThreeLettersCode = "BRL", TwoLettersCode = "BR" },
+            new Language { EnglishName = "BRAZILIAN", ThreeLettersCode = "BRL", TwoLettersCode = "BR" },
+            new Language { EnglishName = "CHINESE", ThreeLettersCode = "CHI", TwoLettersCode = "ZH" },
+            new Language { EnglishName = "CHINESE_SIMP", ThreeLettersCode = "CHI", TwoLettersCode = "CH" },
+            new Language { EnglishName = "CHINESE_TRAD", ThreeLettersCode = "CHI", TwoLettersCode = "CH" },
+            new Language { EnglishName = "SIMPLIFIED_CHINESE", ThreeLettersCode = "CHI", TwoLettersCode = "CH" },
+            new Language { EnglishName = "TRADITIONAL_CHINESE", ThreeLettersCode = "CHI", TwoLettersCode = "CH" },
+            new Language { EnglishName = "CZECH", ThreeLettersCode = "CZE", TwoLettersCode = "CS" },
+            new Language { EnglishName = "DANISH", ThreeLettersCode = "DAN", TwoLettersCode = "DN" },
+            new Language { EnglishName = "DUTCH", ThreeLettersCode = "DUT", TwoLettersCode = "DU" },
+            new Language { EnglishName = "ENGLISH", ThreeLettersCode = "ENG", TwoLettersCode = "EN" },
+            new Language { EnglishName = "ENGLISH", ThreeLettersCode = "USA", TwoLettersCode = "EN" },
+            new Language { EnglishName = "FINNISH", ThreeLettersCode = "FIN", TwoLettersCode = "FI" },
+            new Language { EnglishName = "FRENCH", ThreeLettersCode = "FRE", TwoLettersCode = "FR" },
+            new Language { EnglishName = "GERMAN", ThreeLettersCode = "GER", TwoLettersCode = "GE" },
+            new Language { EnglishName = "GERMAN", ThreeLettersCode = "GER", TwoLettersCode = "DE" },
+            new Language { EnglishName = "GERMAN", ThreeLettersCode = "GER", TwoLettersCode = "GR" },
+            new Language { EnglishName = "HUNGARIAN", ThreeLettersCode = "HUN", TwoLettersCode = "HU" },
+            new Language { EnglishName = "ITALIAN", ThreeLettersCode = "ITA", TwoLettersCode = "IT" },
+            new Language { EnglishName = "JAPANESE", ThreeLettersCode = "JPN", TwoLettersCode = "JP" },
+            new Language { EnglishName = "JAPANESE", ThreeLettersCode = "JAP", TwoLettersCode = "JA" },
+            new Language { EnglishName = "KOREAN", ThreeLettersCode = "KOR", TwoLettersCode = "KO" },
+            new Language { EnglishName = "MALAGASSIAN", ThreeLettersCode = "MAL", TwoLettersCode = "MA" },
+            new Language { EnglishName = "MEXICAN", ThreeLettersCode = "MEX", TwoLettersCode = "ME" },
+            new Language { EnglishName = "MEXICAN_SPANISH", ThreeLettersCode = "MEX", TwoLettersCode = "ME" },
+            new Language { EnglishName = "NORWEGIAN", ThreeLettersCode = "NOR", TwoLettersCode = "NO" },
+            new Language { EnglishName = "POLISH", ThreeLettersCode = "POL", TwoLettersCode = "PL" },
+            new Language { EnglishName = "PORTUGUESE", ThreeLettersCode = "POR", TwoLettersCode = "PT" },
+            new Language { EnglishName = "RUSSIAN", ThreeLettersCode = "RUS", TwoLettersCode = "RU" },
+            new Language { EnglishName = "SPANISH", ThreeLettersCode = "SPA", TwoLettersCode = "SP" },
+            new Language { EnglishName = "SPANISH", ThreeLettersCode = "SPA", TwoLettersCode = "ES" },
+            new Language { EnglishName = "SWEDISH", ThreeLettersCode = "SWE", TwoLettersCode = "SW" },
+            new Language { EnglishName = "TCHINESE", ThreeLettersCode = "CHI", TwoLettersCode = "ZH" },
+            new Language { EnglishName = "TAIWAN", ThreeLettersCode = "TAI", TwoLettersCode = "TW" },
+            new Language { EnglishName = "THAI", ThreeLettersCode = "THA", TwoLettersCode = "TH" },
+            new Language { EnglishName = "THAILAND", ThreeLettersCode = "THA", TwoLettersCode = "TH" },
+            new Language { EnglishName = "TURKISH", ThreeLettersCode = "TUR", TwoLettersCode = "TK" },
+            new Language { EnglishName = "UK_ENGLISH", ThreeLettersCode = "ENG", TwoLettersCode = "UK" },
+            new Language { EnglishName = "GREEK", ThreeLettersCode = "GREE", TwoLettersCode = "GR" },
+        };
+    }
+}
