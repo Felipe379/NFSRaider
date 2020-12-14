75ab71570d6ec9c4733649eaac03f109a367757f
Felipe <46748443+Felipe379@users.noreply.github.com>
2020-12-13 22:56:58 -0300

    - Add UC cars; - Change the logic to generate a few files strings;

---
+++ NFSRaider/GeneratedStrings/Files/Localized/Language.cs
@@ -0,0 +1,9 @@
+﻿namespace NFSRaider.GeneratedStrings.Files.Localized
+{
+    public class Language
+    {
+        public string EnglishName { get; set; }
+        public string ThreeLettersCode { get; set; }
+        public string TwoLettersCode { get; set; }
+    }
+}
