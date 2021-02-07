c81d66d8e70afa1563b8dd47d7ec9b3febc01d77
Felipe <46748443+Felipe379@users.noreply.github.com>
2021-02-07 10:25:33 -0300

    More strings - Add GCareers strings; - Add dumped strings from gameplay files: -- MW: PC/PC Demo, PS2 DEMO, PS2 A124, X360, X360 DEMO; -- C: PC (CARBONO), PS2 DEMO, X360 DEMO, X360 REVIEW; -- PS: PC 1.0, PC 1.1, PS2; -- UC: OLD GEN (WII), XBOX A161, PC 1.0, PC 1.1 -- WORLD: Asian Beta, Build 34, Build 396, Build 1614;

---
+++ NFSRaider/GeneratedStrings/GCareers/BuildgCareers.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NFSRaider.GeneratedStrings.GCareers
+{
+    public class BuildGCareers
+    {
+        public HashSet<string> GetAllGCarrers()
+        {
+            var gCareers = new HashSet<string>(
+                Underground2.List);
+
+            return gCareers;
+        }
+    }
+}
