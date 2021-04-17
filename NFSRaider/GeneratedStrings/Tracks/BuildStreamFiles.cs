e9062ff31c1572d5835557131070787bb2ba3661
Felipe <46748443+Felipe379@users.noreply.github.com>
2021-04-16 23:06:37 -0300

    Add VLT strings and UG2 solids strings

--- NFSRaider/GeneratedStrings/Tracks/BuildStreamFiles.cs
+++ NFSRaider/GeneratedStrings/Tracks/BuildStreamFiles.cs
@@ -1,12 +1,13 @@
 ﻿using NFSRaider.GeneratedStrings.Tracks.CollisionVolumes;
 using NFSRaider.GeneratedStrings.Tracks.EAGLAnimations;
+using NFSRaider.GeneratedStrings.Tracks.EventTriggers;
 using NFSRaider.GeneratedStrings.Tracks.LightSourcesPack;
 using NFSRaider.GeneratedStrings.Tracks.ParameterSets;
 using NFSRaider.GeneratedStrings.Tracks.BarrierGroups;
 using NFSRaider.GeneratedStrings.Tracks.Smokeable;
+using NFSRaider.GeneratedStrings.Tracks.SolidInfos;
 using System.Collections.Generic;
 using System.Linq;
-using NFSRaider.GeneratedStrings.Tracks.EventTriggers;
 
 namespace NFSRaider.GeneratedStrings.Tracks
 {
@@ -21,6 +22,7 @@ namespace NFSRaider.GeneratedStrings.Tracks
                 .Concat(new BuildEventTriggers().GetAllEventTriggers())
                 .Concat(new BuildLightSourcesPack().GetAllLightSourcesPack())
                 .Concat(new BuildBarrierGroups().GetAllSceneryBarrierGroups())
+                .Concat(new BuildSolidInfos().GetAllSolidInfos())
                 .Concat(new BuildSmokeable().GetAllSmokeable())
                 );
 
