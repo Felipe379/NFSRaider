1467f2b4e624bf817db100084d36e9f993a9456a
Felipe <46748443+Felipe379@users.noreply.github.com>
2022-11-11 19:08:46 -0300

    strings

--- NFSRaider/GeneratedStrings/TruncatedStrings/Underground2.cs
+++ NFSRaider/GeneratedStrings/TruncatedStrings/Underground2.cs
@@ -6,11 +6,8 @@ namespace NFSRaider.GeneratedStrings.TruncatedStrings
     {
         public static Dictionary<uint, string> BinHashesTextures = new Dictionary<uint, string>()
         {
-{ 0x0BFD311F, "05_GPTIPS_STEERINGCONTR" },
 { 0x718C780F, "ARC_ETHNIC_SALMONSTUCCO" },
 { 0x9F135643, "VISUAL_PART_REAR_QUARTE" },
-{ 0xA172C631, "01_GPTIPS_PERFORMANCE_T" },
-{ 0xF9C6AB14, "03_GPTIPS_PERFORMANCE_A" },
         };
     }
 }
